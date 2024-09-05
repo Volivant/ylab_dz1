a56badd7430b47a7bd98b22d47d05a4a7a3ab152
Volivant <volivant@yandex.ru>
2024-09-05 21:26:15 +0300

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 import AuthForm from "./components/AuthForm/AuthForm";
-
+import { Server } from 'miragejs';
 import "./App.css";
+
+new Server({
+    routes() {
+      this.namespace = "api";
+      this.get("/users/", () => {
+        return [
+          { user: "User1@example1.com", password: "123" },
+          { user: "User2@example2.com", password: "234" },
+          { user: "User3@example3.com", password: "345" }
+        ];
+      });
+    }
+});
 
 function App() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Server } from 'miragejs';
+
 import './index.css';
 import App from './App';
 
-new Server({
-  routes() {
-    this.namespace = "api";
-    this.get("/users/", () => {
-      return [
-        { user: "User1@example1.com", password: "123" },
-        { user: "User2@example2.com", password: "234" },
-        { user: "User3@example3.com", password: "345" }
-      ];
-    });
-  }
-});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
